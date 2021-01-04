cef5924bcdb6104c75508542db89c0ea6db1030f
Derian Melvin <derianmelvin88@gmail.com>
2021-01-04 23:27:58 +0700

    - Add 'TodoForm' component - Add state hook to 'TodoForm' - Add function variable 'addTodo' to 'App' component - Add function variable 'completeTodo' to 'App' component - Add function variable 'deleteTodo' to 'App' component - Modify 'Todo' props in 'App' component - Modify 'TodoForm' props in 'App' component - Adjust 'Todo' component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React, { useState } from "react";
 import "./App.css";
 
-function Todo({ todo, index }) {
+function Todo({ todo, index, completeTodo, deleteTodo }) {
   return (
-    <div className="todo">
+    <div
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+      className="todo"
+    >
       {todo.text}
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+        <button onClick={() => deleteTodo(index)}>X</button>
+      </div>
     </div>
+  );
+}
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        placeholder="Add Todo..."
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </form>
   );
 }
 
@@ -25,12 +55,38 @@ function App() {
     },
   ]);
 
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
+
   return (
     <div className="App">
       <div className="todo-list">
         {todos.map((todo, index) => {
-          return <Todo key={index} index={index} todo={todo} />
+          return (
+            <Todo
+              key={index}
+              index={index}
+              todo={todo}
+              completeTodo={completeTodo}
+              deleteTodo={deleteTodo}
+            />
+          );
         })}
+        <TodoForm addTodo={addTodo} />
       </div>
     </div>
   );
